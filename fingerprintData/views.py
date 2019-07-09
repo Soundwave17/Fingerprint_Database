8395451311ac4e65b438926abee091c869f8adf4
Leonardo <postaperleonardo@gmail.com>
2019-07-09 17:01:15 +0200

    Code Refactored, added and modified Models to models.py, added faux index view

--- fingerprintData/views.py
+++ fingerprintData/views.py
@@ -2,4 +2,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    response="Hello, world. You're at the index."
+    return HttpResponse(response)
+
+def access(request, question_id):
+    response="You're looking at the access page"
+    return HttpResponse(response)
+
+def purchase(request, question_id):
+    response = "You're looking at the purchase page"
+    return HttpResponse(response)
+
+def checkout(request, question_id):
+    response ="You're looking at the checkout page"
+    return HttpResponse(response)
