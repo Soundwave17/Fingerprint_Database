e32732dce6eaeb0a120ee75290b98ee68003eb55
Leonardo <postaperleonardo@gmail.com>
2019-07-09 12:55:07 +0200

    Code Refactored, added Models to models.py, added faux index view

--- fingerprint/settings.py
+++ fingerprint/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fingerprintData.apps.FingerprintdataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
