5ffede96d9c26088ad7e75aeb3c1d943efa01ad1
Leonardo <postaperleonardo@gmail.com>
2019-07-09 17:31:39 +0200

    Code Refactored, added and modified Models to models.py, added faux index view

--- fingerprintData/views.py
+++ fingerprintData/views.py
@@ -5,14 +5,18 @@ def index(request):
     response="Hello, world. You're at the index."
     return HttpResponse(response)
 
-def access(request, question_id):
+def access(request):
     response="You're looking at the access page"
     return HttpResponse(response)
 
-def purchase(request, question_id):
+def purchase(request):
     response = "You're looking at the purchase page"
     return HttpResponse(response)
 
-def checkout(request, question_id):
+def checkout(request):
     response ="You're looking at the checkout page"
+    return HttpResponse(response)
+
+def overview(request):
+    response = "You're looking at the overview page"
     return HttpResponse(response)
