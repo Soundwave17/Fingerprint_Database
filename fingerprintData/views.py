de1882fddbc634b462c678af584e98128a738c81
Leonardo <postaperleonardo@gmail.com>
2019-07-09 19:51:17 +0200

    Added views, added htmls and css-s. Need contexts on views, and forms for htmls.

--- fingerprintData/views.py
+++ fingerprintData/views.py
@@ -1,22 +1,40 @@
 from django.http import HttpResponse
+from django.template import loader
 
-
+"""
 def index(request):
-    response="Hello, world. You're at the index."
-    return HttpResponse(response)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+"""
 
 def access(request):
-    response="You're looking at the access page"
-    return HttpResponse(response)
+    template = loader.get_template('fingerprintData/access.html')
+    context = [
+
+    ]
+    return HttpResponse(template.render(context, request))
 
 def purchase(request):
-    response = "You're looking at the purchase page"
-    return HttpResponse(response)
+    template = loader.get_template('fingerprintData/purchase.html')
+    context = [
+
+    ]
+    return HttpResponse(template.render(context, request))
 
 def checkout(request):
-    response ="You're looking at the checkout page"
-    return HttpResponse(response)
+    template = loader.get_template('fingerprintData/checkout.html')
+    context = [
+
+    ]
+    return HttpResponse(template.render(context, request))
 
 def overview(request):
-    response = "You're looking at the overview page"
-    return HttpResponse(response)
+    template = loader.get_template('fingerprintData/overview.html')
+    context=[
+
+    ]
+    return HttpResponse(template.render(context, request))
