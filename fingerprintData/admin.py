1d249b5a40ac84ae06f02386af9a76f7ca81d698
Leonardo <postaperleonardo@gmail.com>
2019-07-16 02:13:58 +0200

    Added forms, updated views, added html, templates, libraries and js. Finished placeholder access page. Managed to obtain ajax communication between views. Added admin properties

--- fingerprintData/admin.py
+++ fingerprintData/admin.py
@@ -5,32 +5,32 @@ from .models import Product, Customer, Purchase, PurchaseList, Type, Fingerprint
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('customer_email','customer_name', 'customer_surname', 'customer_register_date')
-    list_filter = ('customer_register_date', 'customer_name', 'customer_surname', 'customer_email')
-    search_fields = ('customer_email', 'customer_name', 'customer_surname','customer_register_date')
+    list_filter = ['customer_register_date']
+    search_fields = ['customer_email', 'customer_name', 'customer_surname','customer_register_date']
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_code', 'product_name', 'product_price', 'product_type', 'product_origin')
-    list_filter = ('product_name', 'product_price', 'product_type', 'product_origin')
-    search_fields = ('product_name', 'product_price', 'product_type', 'product_origin')
+    list_filter = ['product_type', 'product_origin']
+    search_fields = ['product_name', 'product_price', 'product_type', 'product_origin']
 
 class PurchaseAdmin(admin.ModelAdmin):
     list_display = ('purchase_code', 'purchase_customer', 'purchase_date')
-    list_filter = ('purchase_customer', 'purchase_date')
-    search_fields = ('purchase_customer', 'purchase_date')
+    list_filter = ['purchase_date']
+    search_fields = ['purchase_customer', 'purchase_date']
 
 class PurchaseListAdmin(admin.ModelAdmin):
     list_display = ('purchaseList_code', 'purchaseList_product', 'purchaseList_qty')
-    list_filter = ('purchaseList_product', 'purchaseList_qty')
-    search_fields = ('purchaseList_product', 'purchaseList_qty')
+    list_filter = ['purchaseList_product']
+    search_fields = ['purchaseList_product', 'purchaseList_qty']
 
 class TypeAdmin(admin.ModelAdmin):
     list_display = ('type_id', 'type_description')
-    search_fields = ('type_description')
+    search_fields = ['type_description']
 
 
 class FingerprintAdmin(admin.ModelAdmin):
     list_display = ('fingerprint_code', 'fingerprint_data')
-    search_fields = ('fingerprint_data')
+    search_fields = ['fingerprint_data']
 
 
 admin.site.register(Product, ProductAdmin)
