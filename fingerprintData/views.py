1bade265c7d9320a3ba6156ea394953a51aa0f8a
Leonardo <postaperleonardo@gmail.com>
2019-07-17 19:29:07 +0200

    Added overview first graph

--- fingerprintData/views.py
+++ fingerprintData/views.py
@@ -61,106 +61,6 @@ class prova(View):
         template='fingerprintData/prova.html'
         return render (request, template)
 
-"""
-class Basic_CRUD_Create_Form(ModelForm):
-        "
-    Render the basic crud create form
-        "
-    full_name = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control',
-                                      'placeholder': 'Enter your full name',
-                                      'maxlength': '75'}))
-
-    email = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control',
-                                      'placeholder': 'Enter your working email',
-                                      'maxlength': '254'}))
-
-    subject = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'class': 'form-control', 'placeholder': 'Enter your subject',
-            'maxlength': '75'}))
-
-    message = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control textarea',
-                                     'placeholder': 'Enter your message'}))
-
-    class Meta:
-        model = ContactUs
-        fields = ('full_name', 'email', 'subject', 'message')
-
-def basic_crud_create_view(request):
-    "Renders the basic crud create operation."
-    if request.method == 'GET':
-
-        # Get contact us form to display
-        form = Basic_CRUD_Create_Form()
-        return render(request, 'myroot/basic_crud_create.html',
-                      {'form': form,
-                       'title': "Django Basic CRUD: CREATE New Row with Actual Example",
-                       'meta_desc': ""Learn Django basic CRUD to create a new rows and store it on dev_contact_us database table - "" + settings.SITE_FULL_NAME})
-
-    data = dict()
-    if request.method == 'POST':
-        # Get the form data
-        form = Basic_CRUD_Create_Form(request.POST)
-
-        if form.is_valid():
-            form.save()  # insert new row
-
-            # Return some json response back to the user
-            msg = "" Your data has been inserted successfully, thank you! ""
-            data = dict_alert_msg('True', 'Awesome!', msg, 'success')
-
-        else:
-
-            # Extract form.errors
-            msg = None
-            msg = [(k, v[0]) for k, v in form.errors.items()]
-            data = dict_alert_msg('False', 'Oops, Error', msg, 'error')
-
-        return JsonResponse(data)
-"""
-
-"""
-def username_exists(request):
-    data = {'msg':''}   
-    if request.method == 'GET':
-        username = request.GET.get('username').lower()
-        exists = Usernames.objects.filter(name=username).exists()
-        if exists:
-            data['msg'] = username + ' already exists.'
-        else:
-            data['msg'] = username + ' does not exists.'
-    return JsonResponse(data)
-"""
-
-"""
-def create_post(request):
-    if request.method == 'POST':
-        post_text = request.POST.get('the_post')
-        response_data = {}
-
-        post = Post(text=post_text, author=request.user)
-        post.save()
-
-        response_data['result'] = 'Create post successful!'
-        response_data['postpk'] = post.pk
-        response_data['text'] = post.text
-        response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-        response_data['author'] = post.author.username
-
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
-    else:
-        return HttpResponse(
-            json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json"
-        )
-"""
-
 
 #TODO instead of password checking, use fingerprint checking.
 
@@ -193,9 +93,6 @@ def customer_exists(request):
 
     return JsonResponse(data)
 
-
-
-
 def create_customer(request):
     data = dict()
     if request.method == 'POST':
@@ -215,6 +112,38 @@ def create_customer(request):
 
         return JsonResponse(data)
 
+def get_revenue_by_year(request,customer_email):
+    customer=get_object_or_404(Customer, pk=customer_email)
+    if request.method == 'GET':
+        data= {'Jan': 0, 'Feb': 0, 'Mar':0, 'Apr':0,'May':0,'Jun':0,
+               'Jul':0, 'Aug':0, 'Sep':0, 'Oct':0, 'Nov':0, 'Dec':0 }
+        dataArray=[0,0,0,0,0,0,0,0,0,0,0,0]
+        year = request.GET.get('year')
+        tot_list = Purchase.objects.filter(purchase_date__year=year)
+        for m in tot_list:
+            p_code=m.purchase_code
+            purchase_list= PurchaseList.objects.filter(purchaseList_code=p_code)
+            for p in purchase_list:
+                product= Product.objects.get(product_code=p.purchaseList_product.product_code)
+                dataArray[m.purchase_date.month - 1] += (product.product_price)*(p.purchaseList_qty)
+        data['Jan'] = dataArray[0]
+        data['Feb'] = dataArray[1]
+        data['Mar'] = dataArray[2]
+        data['Apr'] = dataArray[3]
+        data['May'] = dataArray[4]
+        data['Jun'] = dataArray[5]
+        data['Jul'] = dataArray[6]
+        data['Aug'] = dataArray[7]
+        data['Sep'] = dataArray[8]
+        data['Oct'] = dataArray[9]
+        data['Nov'] = dataArray[10]
+        data['Dec'] = dataArray[11]
+
+        return JsonResponse(data)
+
+
+
+"""
 def create_purchase(request):
     data = dict()
     if request.method == 'POST':
@@ -280,3 +209,4 @@ def create_fingerprint(request):
             data = globalfunctions.dict_alert_msg('False', 'Oops, Error', msg, 'error')
 
         return JsonResponse(data)
+"""
