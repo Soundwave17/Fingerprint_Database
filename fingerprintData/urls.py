877a9b6e503e058669a6b7b4072f444834f0c58e
Exelann <exelan97@gmail.com>
2019-07-23 19:44:05 +0200

    \1

--- fingerprintData/urls.py
+++ fingerprintData/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('customer_login/', views.customer_login, name='customer_login'),
     path('customer_exists/', views.customer_exists, name='customer_exists'),
     path('<str:customer_email>/overview/',views.overview.as_view() , name='overview'),
+    path('<str:customer_email>/overview/get_revenue_by_year/', views.get_revenue_by_year, name='revenue'),
     path('<str:customer_email>/purchase/', views.purchase.as_view(), name='purchase'),
     path('<str:customer_email>/purchase/checkout', views.checkout.as_view(), name='checkout'),
     path('register/create_customer/', views.create_customer, name='create_customer'),
